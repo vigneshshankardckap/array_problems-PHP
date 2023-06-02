e732ccb1e0de6192403a0a48fff48fe4d4b4cc7d
vigneshshankardckap <vigneshshankardckap@gmail.com>
2023-06-02 11:06:38 +0530

    solved

---
+++ arrayCombine.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+$keys = array(
+    "field1"=>"vignesh",
+    "field2"=>"kishore",
+    "field3"=>"deepak"
+);
+
+$values = array(
+    "field1value"=>"22",
+    "field2value"=>"20",
+    "field3value"=>"21"
+);
+
+//this function creates an array by using the elements from one "keys" array and one "values" array.
+
+$keyValue=array_combine($keys,$values);
+
+print_r($keyValue);
